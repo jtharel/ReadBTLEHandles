0644b466c317f33515c765899de8382073e5c1c1
pooneth <jtharel@yahoo.com>
2018-08-16 12:59:08 -0600

    Add files via upload

---
+++ read_all_handles.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import sys
+import subprocess
+import binascii
+
+def Help():
+    print("Usage:")
+    print("  read_all_handles.py [Remote Bluetooth Hardware Address]")
+    print(" ")
+    print("  read_all_handles.py 00:01:02:03:04:05")
+
+
+def Gatttool(HWADDR):
+    print("BTLE Hardware addresss: " + HWADDR)
+    proc = subprocess.Popen(["gatttool", "-b", HWADDR, "--char-desc"], stdout=subprocess.PIPE)
+    while True:
+        line = proc.stdout.readline()
+        print("*"*50)
+        if line != '':
+            handle = line[9:15]
+            print("Handle: " + handle)
+            newproc = subprocess.Popen(["gatttool", "-b", HWADDR, "--char-read", "-a", handle], stdout=subprocess.PIPE)
+            while True:
+                newline = newproc.stdout.readline()
+                newline = newline.rstrip()
+                if newline != '':
+                    print(newline)
+                    ascii = binascii.unhexlify(newline[32:].replace(" ", ""))
+                    print("Ascii: " + ascii)
+                    
+                else:
+                    break
+
+        else:
+            break
+
+if len(sys.argv) == 1:
+    Help()
+    sys.exit()
+
+if len(sys.argv[1]) == 17:
+    HWADDR = sys.argv[1]
+    Gatttool(HWADDR)
+else:
+    Help()
+
